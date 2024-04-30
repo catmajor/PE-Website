3b85608351c15784730faa3c7068e3d3e7689b6d
catmajor <119273404+catmajor@users.noreply.github.com>
2024-04-30 13:23:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector("nav");
+const navWrapper = document.querySelector("#nav-wrapper");
+const placeholder = document.getElementById("nav-placeholder")
+console.log(placeholder)
+
+function updateHeader() {
+  // 54vh + 12px - (12vh + 8px) = 42vh + 4px
+  let scrollRatio = (window.innerHeight*41/100 + 8 - window.scrollY)/(window.innerHeight*41/100 + 8);
+  scrollRatio = scrollRatio<=0?0:scrollRatio;
+  navWrapper.style.setProperty("--scrollRatio", `${scrollRatio}`);
+}
+
+updateHeader();
+window.addEventListener("scroll", updateHeader);
