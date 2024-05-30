0ad7b0bd72820ecb1fd33df026ebffebe8fdf8b2
catmajor <119273404+catmajor@users.noreply.github.com>
2024-05-30 16:41:51 +0000

    final changes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require_once('couch/cms.php'); ?>
 <cms:repeatable name="sections" label="Insert Sections">
   <cms:editable name="section-label" type="text" label="Section Label, will be displayed as a header"/>
+  <cms:editable name="section-name" type="text" label="A unique_id to assign each element. Cannot be blank for element to be created"/>
 </cms:repeatable>
 <cms:func 'make-cms-backend' name='' labelText=''>
   <cms:repeatable name="<cms:show name/>_item" label="Items for <cms:show labelText/> Section ">
@@ -9,14 +10,13 @@
     <cms:editable name="<cms:show name/>_item_title" type="text" label="<cms:show labelText/> - Title - will only show if text is set to show. First appearing title in either text or hidden element will be used for the anchor in the menu for this item">
       Title
     </cms:editable>
-    <cms:editable name="<cms:show name/>_item_text" type="text" label="<cms:show labelText/> - Text - will only show if text is set to show">
-      Text
+    <cms:editable name="<cms:show name/>_item_text" type="text" label="<cms:show labelText/> - Text - will only show if text is set to show, or the unique_id to link to a staff database for staff elements">
     </cms:editable>
     <cms:editable name="<cms:show name/>_item_img" label="<cms:show labelText/> - Image - will only show if image is set to show" type="image" hidden='1'></cms:editable>
   </cms:repeatable>
 </cms:func>
 <cms:func 'make-staff-backend' parent='' labelText='' >
-  <cms:repeatable name="<cms:show parent/>_staff" label="Insert Staff Information Here for parent <cms:show labelText/>">
+  <cms:repeatable name="<cms:show parent/>_staff" label="Insert Staff Information Here for a(n) <cms:show labelText/> staff section. Id: <cms:show parent/>">
     <cms:editable name="<cms:show parent/>_staff_name" type="text" label="Staff Name">name</cms:editable>
     <cms:editable name="<cms:show parent/>_staff_school" type="text" label="Staff School/Grade Level">ABRHS</cms:editable>
     <cms:editable name="<cms:show parent/>_staff_contact" type="text" label="Staff Contact">staff@abschools.org</cms:editable>
@@ -45,9 +45,10 @@
               <cms:set cur_id = "<cms:get item_section_id />"/>
         </cms:if>
         <cms:set image_text_switch = "<cms:get item_show_image_or_text/>"/>
-        <cms:if image_text_switch = "staff">
-          <cms:set staff_id="<cms:concat name '_item_' k_count />"/>
-          <cms:set staff_label="<cms:concat labelText '_' k_count />"/>
+        <cms:if image_text_switch = "staff" && item_text != ''>
+          <cms:set unique_staff_id = "<cms:get item_text/>"/>
+          <cms:set staff_id="<cms:concat name '_item-' unique_staff_id />"/>
+          <cms:set staff_label="<cms:concat labelText />"/>
           <cms:call 'make-staff-backend' staff_id staff_label/>
           <cms:set staff_image = "<cms:concat staff_id '_staff_image'/>"/>
           <cms:set staff_school = "<cms:concat staff_id '_staff_school'/>"/>
@@ -105,28 +106,30 @@
     </ul>
   </li>
 </cms:func>
-<cms:show_repeatable 'sections'>
-  <cms:call 'make-cms-backend' k_count section-label/>
-</cms:show_repeatable>
 <!DOCTYPE html>
 <html>
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>ABRHS PE/HE</title>
-  <link href="img/logo_light.png" rel="icon">
+  <cms:editable name="icon_img" type="image" label="Image to show in the tab" hidden="1"/>
+  <link href="<cms:show icon_img/>" rel="icon">
+  <title><cms:editable name="title_text" type="text" label="Text to show up in title of website">ABRHS PE/HE</cms:editable></title>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Questrial" rel="stylesheet">
   <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
+  <cms:show_repeatable 'sections'>
+    <cms:call 'make-cms-backend' section-name section-label/>
+  </cms:show_repeatable>
   <div id="nav-wrapper" class="nav-vars">
     <nav>
       <div id="items-wrapper">
         <div id="items">
-          <figure><img src="img/logo.png"></figure>
-          <h1><span class="hidden">AB</span>RHS PE/HE</h1>
+          <cms:editable name="header_img" type="image" label="Image to show for header icon" hidden="1"/>
+          <figure><img src="<cms:show header_img/>"></figure>
+          <h1><span class="hidden">AB</span><cms:editable type="text" name="header_title" label="Text to display in the header">RHS PE/HE</cms:editable></h1>
         </div>
       </div>
     </nav>
@@ -134,7 +137,7 @@
     <div id="menu-wrapper">
       <ul id="menu">
         <cms:show_repeatable 'sections'>
-          <cms:call 'make-nav' k_count section-label/>
+          <cms:call 'make-nav' section-name section-label/>
         </cms:show_repeatable>
         <li><a href="https://abrhs.abschools.org/"> ABRHS </a>
         </li>
@@ -148,7 +151,7 @@
        <h2>Acton Boxborough Physical Education</h2>
     </div>
     <cms:show_repeatable 'sections'>
-      <cms:call 'make-section' k_count section-label/>
+      <cms:call 'make-section' section-name section-label/>
     </cms:show_repeatable>
   </main>
   <footer>
@@ -169,7 +172,8 @@
       <p class="address">
         36 Charter Road, Acton, MA01720 <br>
         Phone 978-264-4700 <br>
-        Powered by <a href="https://www.couchcms.com/">Couch CMS</a>
+        Powered by <a href="https://www.couchcms.com/">Couch CMS</a><br>
+        Source Code <a href="https://github.com/catmajor/PE-Website/tree/main">Here</a>
       </p>
     </div>
   </footer>
